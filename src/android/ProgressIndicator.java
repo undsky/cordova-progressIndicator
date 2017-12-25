b9dbcabc641360f9b8eac9a453812a22e4a1909f
姜彦汐 <jiangyanxi@live.com>
2017-12-25 16:52:24 +0800

    no message

--- src/android/ProgressIndicator.java
+++ src/android/ProgressIndicator.java
@@ -6,10 +6,23 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.app.ProgressDialog;
 
+import android.content.DialogInterface;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.kaopiz.kprogresshud.KProgressHUD;
+
 public class ProgressIndicator extends CordovaPlugin {
 
     private ProgressDialog progressIndicator = null;
-	private static final String LOG_TAG = "ProgressIndicator";
+    private static final String LOG_TAG = "ProgressIndicator";
+    private KProgressHUD hud;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -34,7 +47,7 @@ public class ProgressIndicator extends CordovaPlugin {
             return true;
         } else if (action.equals("showText")) {
             String title = args.getString(1);
-			String text = args.getString(2);
+            String text = args.getString(2);
             show(title, text, false);
             callbackContext.success();
             return true;
@@ -42,9 +55,29 @@ public class ProgressIndicator extends CordovaPlugin {
             hide();
             callbackContext.success();
             return true;
+        } else if (action.equals("showDeterminate")) {
+            Integer timeout = args.getInt(1);
+            String title = args.getString(2);
+            hud = KProgressHUD.create(cordova.getActivity()).setStyle(KProgressHUD.Style.PIE_DETERMINATE)
+                    .setLabel(title);
+            hud.show();
+            simulateProgressUpdate(timeout);
+            callbackContext.success();
+            return true;
+        } else if (action.equals("showDeterminateWithLabel")) {
+
+        } else if (action.equals("showAnnular")) {
+
+        } else if (action.equals("showAnnularWithLabel")) {
+
+        } else if (action.equals("showBar")) {
+
+        } else if (action.equals("showBarWithLabel")) {
+
         } else {
-            callbackContext.error("Not supported call. On Android we only support show, showSimple, showSimpleWithLabel and showSimpleWithLabelDetail");
-		}
+            callbackContext.error(
+                    "Not supported call. On Android we only support show, showSimple, showSimpleWithLabel and showSimpleWithLabelDetail");
+        }
 
         return false;
     }
@@ -56,7 +89,7 @@ public class ProgressIndicator extends CordovaPlugin {
      */
     public void show() {
         progressIndicator = new ProgressDialog(cordova.getActivity());
-		progressIndicator.show();
+        progressIndicator.show();
     }
 
     /**
@@ -66,8 +99,8 @@ public class ProgressIndicator extends CordovaPlugin {
      */
     public void show(String text) {
         progressIndicator = new ProgressDialog(cordova.getActivity());
-		progressIndicator.setTitle(text);
-		progressIndicator.show();
+        progressIndicator.setTitle(text);
+        progressIndicator.show();
     }
 
     /**
@@ -77,10 +110,10 @@ public class ProgressIndicator extends CordovaPlugin {
      */
     public void show(String title, String detail, Boolean withTitle) {
         progressIndicator = new ProgressDialog(cordova.getActivity());
-		if(withTitle)
-			progressIndicator.setTitle(title);
-		progressIndicator.setMessage(detail);
-		progressIndicator.show();
+        if (withTitle)
+            progressIndicator.setTitle(title);
+        progressIndicator.setMessage(detail);
+        progressIndicator.show();
     }
 
     /**
@@ -91,5 +124,35 @@ public class ProgressIndicator extends CordovaPlugin {
             progressIndicator.dismiss();
             progressIndicator = null;
         }
+    }
+
+    private void simulateProgressUpdate(Integer timeout) {
+        hud.setMaxProgress(timeout);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            int currentProgress;
+
+            @Override
+            public void run() {
+                currentProgress += 1;
+                hud.setProgress(currentProgress);
+                // if (currentProgress == 80) {
+                //     hud.setLabel("Almost finish...");
+                // }
+                if (currentProgress < timeout) {
+                    handler.postDelayed(this, 50);
+                }
+            }
+        }, 100);
+    }
+
+    private void scheduleDismiss() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hud.dismiss();
+            }
+        }, 2000);
     }
 }
